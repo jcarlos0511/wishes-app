bab0534a224d67bb0838a158ed424ae1d17dc80f
Chara- <escor05_11@hotmail.com>
2020-03-24 21:25:09 -0500

    adding 'add item'

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { WhishesService } from 'src/app/services/wishes.service';
+import { ActivatedRoute } from '@angular/router';
+import { Lista } from 'src/app/models/lista.model';
+import { ListaItem } from 'src/app/models/lista-item.model';
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarPage implements OnInit {
 
-  constructor() { }
+lista: Lista;
+nombreItem = '';
+
+  constructor( private wishesService: WhishesService, private route: ActivatedRoute ) { 
+
+    
+
+    const listaId = this.route.snapshot.paramMap.get('listaId');
+    //console.log(listaId);
+
+    this.lista = this.wishesService.obtenerLista( listaId);
+    //console.log(this.lista);
+
+   }
 
   ngOnInit() {
+  }
+
+  agregarItem(){
+    if (this.nombreItem.length === 0) {
+      return;
+    }
+    const nuevoItem = new ListaItem(this.nombreItem);
+
+    this.lista.items.push( nuevoItem);
+
+    this.nombreItem = '';
+    
+    this.wishesService.guardarStorage();
+  }
+
+  cambioCheck( item: ListaItem ){
+    //console.log(item);
+    const pendientes = this.lista.items.filter( itemData => !itemData.completado ).length;
+
+    console.log(pendientes);
+
+    this.wishesService.guardarStorage();
   }
 
 }
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,23 +1,58 @@
-import { Component } from '@angular/core';
-import { WhishesService } from 'src/app/services/wishes.service';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { WhishesService } from "src/app/services/wishes.service";
+import { Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
+  constructor(
+    public whishesService: WhishesService,
+    private router: Router,
+    private alertCrtl: AlertController
+  ) {}
 
-  
-
-  constructor( public whishesService: WhishesService, private router: Router) {
+  async agregarLista() {
     
-    
-  }
+    const alert = await this.alertCrtl.create({
+      header: "Nueva Lista",
+      inputs: [
+        {
+          name: "titulo",
+          type: "text",
+          placeholder: "Nombre de la lista"
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: ( data ) => {
 
-  agregarLista(){
-    this.router.navigateByUrl('tabs/tab1/agregar');
-  }
+            console.log(data);
 
+            if( data.titulo.length === 0 ){
+              return;
+            }
+            // Crear la lista
+
+            const listaId = this.whishesService.crearLista( data.titulo );
+
+            this.router.navigateByUrl(`tabs/tab1/agregar/${ listaId }`);
+
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
